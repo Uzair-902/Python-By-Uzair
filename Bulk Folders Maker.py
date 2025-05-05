25f9ac51a71f9ba4506a1e46bb79aa69b5c37eb2
Uzair-902 <uzairshah4270@gmail.com>
2025-05-05 15:26:33 +0500

    Bulk Folders Maker
    Bulk Folder Creator & Remover – Python Script
    
    A simple Python script to bulk create or delete folders via terminal input. Useful for organizing project directories or testing.
    
    Features
    
    Create multiple folders with a custom name.
    
    Delete a specific folder by path.
    
    Simple CLI interface.
    
    Error handling included.
    
    Clean exit with a custom message.

---
+++ Bulk Folders Maker.py
@@ -0,0 +1,38 @@
+#main infinite loop
+while True:
+ # Importing the os module
+ import os
+ 
+ #adding a try-except block for error handling.
+ try:
+ 	#getting path for operation
+ 	path = input('Enter path for operation, "q" to exit. :')
+ 	#exit logic
+ 	if path.lower() == 'q':
+ 		print('Programmed By Uzair_902. Thanks For Using. Also Support me at https://www.youtube.com/@FixDroid-Pro')
+ 		break
+ 	#getting prompt
+ 	prompt = input('What do you want to do? Delete/Create?')
+ 
+ 	#conditional statements
+ 	if prompt.lower() == 'delete':
+ 		os.rmdir(path)
+ 	elif prompt.lower() == 'create':
+ 		os.chdir(path)
+ 	
+		
+ 		#getting name for folders
+ 		name = input('Enter Name for folders:')
+		
+ 		#getting quantity of folders
+ 		qty = int(input('Enter qty:'))
+
+ 		#main loop to create bulk folders
+ 		for i in range(qty+1):
+ 			os.mkdir(f"{name}, {i}")
+ 	else:
+ 		print('Error, Please Input Valid Prompt!')
+
+ #handling exceptions and errors
+ except Exception as e:
+ 	print(f"\nAn Unknown Error ({e}) occured. Pelase Make Sure The Input Is Correct and Try Again. \n")
